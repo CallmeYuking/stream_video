311f5aa01e6929eafd7db95373be3cd06dde60d7
Yuking Quan <yuking@Yukings-MacBook-Pro.local>
2020-04-19 19:16:48 -0700

    connecting Auth with Action Creators.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut} from '../actions';
 
 class GoogleAuth extends React.Component {
     state = { isSignedIn: null };
@@ -15,8 +17,12 @@ class GoogleAuth extends React.Component {
         });
     }
 
-    onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    onAuthChange = isSignedIn => {
+        if(isSignedIn) {
+            this.props.signIn();
+        } else {
+            this.props.signOut();
+        }
     }
 
     onSignInClick = () => {
@@ -39,7 +45,7 @@ class GoogleAuth extends React.Component {
             );
         } else {
             return (
-                <button onClick={this.onSignInClickonSignOutClick} className="ui blue google button">
+                <button onClick={this.onSignInClick} className="ui blue google button">
                     <i className="google icon" />
                     Sign in with Google
                 </button>
@@ -52,4 +58,4 @@ class GoogleAuth extends React.Component {
     }
 }
 
-export default GoogleAuth;
+export default connect(null, { signIn, signOut })(GoogleAuth);
